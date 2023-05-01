1c53afe7becba52fd28f5872bef52e418787260d
aa732396 <tyrone.king@aa.com>
2023-05-01 11:00:01 -0500

    Changed YML name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("Hello world!1");
+        System.out.println("Hello world!2");
+        System.out.println("Hello world!3");
+        System.out.println("Hello world!4");
     }
 }
